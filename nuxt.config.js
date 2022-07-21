837838dbe472b5dddc52a09d0e83a26d0f4fda9d
Jordaine Gayle <freshcode9@gmail.com>
2022-07-21 17:33:51 -0500

    .....

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,24 +11,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'vuesax/dist/vuesax.css'
-  ],
+  css: ['vuesax/dist/vuesax.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/signalr.js',
-    '@/plugins/crypto.js',
-    '@/plugins/vuesax'
-  ],
+  plugins: ['@/plugins/signalr.js', '@/plugins/crypto.js', '@/plugins/vuesax'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -49,17 +41,45 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://e2e.azurewebsites.net'
+    baseURL: 'https://e2e.azurewebsites.net',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      theme_color: '#FA003F',
+      background_color: '#ffffff',
+      display: 'fullscreen',
+      scope: '/',
+      start_url: '/',
+      name: 'ENCHAT',
+      short_name: 'ECHAT',
+      description: 'This is an end to end chant application',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-256x256.png',
+          sizes: '256x256',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
